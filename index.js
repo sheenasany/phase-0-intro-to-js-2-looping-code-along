9157757b2d5030981e1c37b4c9f0a67b7e54ec15
Sheena Sang <sheenasany@gmail.com>
2022-06-14 18:53:42 -0400

    commit tree 38b45cc396ab735ceac59ec7d7ae61f437727768

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const name = ["Guadalupe", "Oli", "Aki"]
+const event = surprise
+
+function writeCards(name, event) {
+    let messages = []
+     for (let i = 0; i < name.length; i++) {
+        messages.push (`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    };
+        return messages
+    };
+
+    function countDown(int) {
+        let i = 10
+        while (i >= 0) {
+            console.log(i--);
+        }; 
+    };
+
